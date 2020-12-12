6e40ea76dec6af0eb1dcde828f83e66621147649
Sean McBane <sean.mcbane@protonmail.com>
2020-12-12 14:47:43 -0600

    Better multiplication test

--- test/multiplication.cpp
+++ test/multiplication.cpp
@@ -22,3 +22,24 @@ TEST_CASE("Squaring a single-variable polynomial")
     REQUIRE(poly2.coeffs()[3] == -2);
     REQUIRE(poly2.coeffs()[4] == 1);
 }
+
+TEST_CASE("A more complicated, multi-variable test case")
+{
+    constexpr auto powers = PowersList<Powers<0, 0>, Powers<0, 1>, Powers<1, 0>, Powers<1, 1>>{};
+    constexpr auto coeffs1 = std::array<int, 4>{ 2, 1, 2, 3 };
+    constexpr auto coeffs2 = std::array<int, 4>{ 4, 3, 2, 1 };
+    constexpr auto poly1 = make_poly(coeffs1, powers);
+    constexpr auto poly2 = make_poly(coeffs2, powers);
+    constexpr auto result = poly1 * poly2;
+
+    static_assert(result.num_terms == 9);
+    REQUIRE(result.coeffs()[0] == 8);
+    REQUIRE(result.coeffs()[1] == 10);
+    REQUIRE(result.coeffs()[2] == 3);
+    REQUIRE(result.coeffs()[3] == 12);
+    REQUIRE(result.coeffs()[4] == 22);
+    REQUIRE(result.coeffs()[5] == 10);
+    REQUIRE(result.coeffs()[6] == 4);
+    REQUIRE(result.coeffs()[7] == 8);
+    REQUIRE(result.coeffs()[8] == 3);
+}
